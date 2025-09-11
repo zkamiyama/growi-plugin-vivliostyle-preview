24e7797e26cc065f6f92fd623d0cc17dfff37c6d
zki <kmzk.metatron@gmail.com>
2025-09-11 22:59:19 +0900

    chore: remove archived stubs from src

--- src/ui/VivliostylePlanB.tsx
+++
@@ -1,4 +0,0 @@
-// This file has been archived to `archive/unused` because static analysis found no references.
-// If you need to restore it, copy from `archive/unused/VivliostylePlanB.tsx`.
-
-export default null;
